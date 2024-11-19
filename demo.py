cd6bdd2205eb20407ad951bca71a6482cfdfe711
Logan Endes <log@jet.csh.rit.edu>
2024-11-19 02:52:33 -0500

    more interactive plus easier setup

--- demo.py
+++ demo.py
@@ -3,20 +3,30 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 
 def main():
     """"""
-    device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
     
+    model_options = ["microsoft/Phi-3-medium-128k-instruct", "microsoft/Phi-3-mini-128k-instruct", "facebook/opt-2.7b",
+                     "facebook/opt-6.7b",  "infly/OpenCoder-8B-Instruct"]
+    
+    for m in model_options:
+        print(m)
+
     model_name = input("Choose a model (must be from transformers like facebook/opt-350m): ")
     
-    inpt = input("Give an input for the model: ")
     
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name).to(device)
     
-    input_ids = tokenizer(inpt, return_tensors="pt").input_ids.to(device)
+    while True:    
+        inpt = input("Give an input for the model: ")
+        if inpt.lower() == 'x' or inpt.lower() == 'q':
+            break
+        token_inpt = int(input("How many tokens would you like to generate? "))
+        input_ids = tokenizer(inpt, return_tensors="pt").input_ids.to(device)
 
-    outputs = model.generate(input_ids, max_new_tokens=50)
-    output = tokenizer.decode(outputs[0])
-    print(output)
+        outputs = model.generate(input_ids, max_new_tokens=token_inpt)
+        output = tokenizer.decode(outputs[0])
+        print(output)
     
 if __name__ == "__main__":
     main()
---
+++ setup_environment.sh
@@ -0,0 +1,8 @@
+pip install jupyter transformers
+pip install packaging
+pip install wheel
+pip install ninja
+pip install setuptools
+pip install flash-attn --no-build-isolation
+pip install accelerate
+pip install sentencepiece
